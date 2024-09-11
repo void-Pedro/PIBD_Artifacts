8064a3d30d6fc836470bacc583a0518063dbddd1
Annalice <101228884+Annalicefs@users.noreply.github.com>
2024-09-11 14:42:59 -0300

    Update DeletaVeiculo.sql

--- Stored Procedures/DeletaVeiculo.sql
+++ Stored Procedures/DeletaVeiculo.sql
@@ -9,7 +9,8 @@ CREATE TABLE IF NOT EXISTS carro (
 );
 */
 
---Exclusão lógica de veículo se não estiver associado a uma viagem futura
+-Exclusão lógica de veículo se não estiver associado a uma viagem futura
+
 CREATE OR REPLACE PROCEDURE DeletaVeiculo(
     RENAVAM IN NUMBER)
 IS
@@ -18,14 +19,14 @@ BEGIN
 	SELECT COUNT(*) INTO cont /* Verificando se existe viagem futura associado ao veículo) */
     FROM Viagem v
 	WHERE v.Renavam = RENAVAM 
-    	AND v.Data_saida >= TRUNC(SYSDATE)
-    	AND v.Horario_saida > TO_CHAR(SYSDATE, 'HH24:MI:SS');
+    	AND v.Data_saida >= SYDATE
+    	AND	v.Horario_saida > TO_CHAR(SYSDATE, 'HH24:MI:SS');
 
 	IF cont > 0 THEN
         RAISE_APPLICATION_ERROR(-20001, 'Não é possivel excluir o veiculo pois está associado a futuras viagens');
     ELSE
     	UPDATE Carro c /* Excluindo carro */
-       	SET c.Deletado = 'T'
+       	SET c.Deletado = TRUE
 		WHERE c.Renavam = RENAVAM;
 
         DBMS_OUTPUT.PUT_LINE('Carro excluido com sucesso');
