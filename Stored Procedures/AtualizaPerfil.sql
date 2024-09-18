e516c2a8d3f10683bdd1734a893e9e22ed599a46
Annalice <101228884+Annalicefs@users.noreply.github.com>
2024-09-18 12:32:37 -0300

    Update AtualizaPerfil.sql

--- Stored Procedures/AtualizaPerfil.sql
+++ Stored Procedures/AtualizaPerfil.sql
@@ -1,19 +1,21 @@
 CREATE OR REPLACE PROCEDURE AtualizaPerfil(
-    ID_usuario IN NUMBER,
-    nRedeSocial IN VARCHAR2,
-    nDDI IN NUMBER,
-    nDDD IN NUMBER,
-    nNumTelefone IN NUMBER)
-IS
+    @ID_usuario INT,
+    @RedeSocial NVARCHAR(100) = NULL,
+    @DDI INT = NULL,
+    @DDD INT = NULL,
+    @NumTelefone INT = NULL)
+AS
 BEGIN
-	UPDATE Usuario_Geral
-	SET RedeSocial = COALESCE(nRedeSocial, RedeSocial),
-		DDI = COALESCE(nDDI, DDI),
-		DDD = COALESCE(nDDD, DDD),
-		num_telefone = COALESCE(nNumTelefone, num_telefone)
-	WHERE ID = ID_usuario;
+	BEGIN TRY
+		UPDATE Usuario_Geral
+		SET rede_social = COALESCE(@RedeSocial, rede_social),
+			DDI = COALESCE(@DDI, DDI),
+			DDD = COALESCE(@DDD, DDD),
+			num_telefone = COALESCE(@NumTelefone, num_telefone)
+		WHERE ID = @ID_usuario;
+	END TRY
 
-EXCEPTION 
-    WHEN OTHERS THEN
-    	DBMS_OUTPUT.PUT_LINE('Erro ao atualizar perfil ' || SQLERRM);
+	BEGIN CATCH
+		PRINT 'Erro ao atualizar perfil: ' + ERROR_MESSAGE();
+    END CATCH
 END;
