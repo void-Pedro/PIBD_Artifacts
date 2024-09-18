de5c062216e4ca4f88e6679c465c4ae7f6d94a9d
Annalice <101228884+Annalicefs@users.noreply.github.com>
2024-09-18 13:07:40 -0300

    Update trg_Atualiza_Vaga.sql

--- Trigger DML/trg_Atualiza_Vaga.sql
+++ Trigger DML/trg_Atualiza_Vaga.sql
@@ -1,3 +1,4 @@
+/*
 CREATE OR REPLACE TRIGGER trg_Atualiza_Vaga
 	AFTER INSERT 
     ON Caroneiro_viagem
@@ -7,4 +8,15 @@ BEGIN
 	SET Numero_max_pessoas = Numero_max_pessoas - 1
 	WHERE ID_viagem = :NEW.ID_viagem;
 END;
-/
+*/
+
+CREATE TRIGGER trg_Atualiza_Vaga
+    ON caroneiro_viagem
+	AFTER INSERT 
+AS
+BEGIN
+	UPDATE viagem
+	SET numero_max_pessoas = numero_max_pessoas - 1
+	FROM viagem v
+	INNER JOIN INSERTED i ON v.ID_viagem = i.ID_viagem;
+END;
