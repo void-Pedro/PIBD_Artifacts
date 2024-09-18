764f9738adab71d7a2a42a7a7a3e90fc060f20ef
Alessandro André Pérez Rodríguez <108166086+aaprodri@users.noreply.github.com>
2024-09-18 09:10:39 -0300

    Update brasileiro.sql

--- Tables/brasileiro.sql
+++ Tables/brasileiro.sql
@@ -1,8 +1,9 @@
 --Tabela Brasileiro
 
 CREATE TABLE IF NOT EXISTS brasileiro (
-	ID_usuario INT NOT NULL,
+	ID_usuario INT IDENTITY (1,1) NOT NULL,
 	CPF INT NOT NULL,
+	Senha VARCHAR (50) NOT NULL,
 
 	PRIMARY KEY(CPF),
 	FOREIGN KEY(ID_usuario) REFERENCES usuario_geral(ID)
