5963f1ad3661d3f26c4fbdedd4d4cf738a6d69ee
Annalice <101228884+Annalicefs@users.noreply.github.com>
2024-09-18 15:17:47 -0300

    Update BuscarCarona.sql

--- Functions/BuscarCarona.sql
+++ Functions/BuscarCarona.sql
@@ -19,57 +19,23 @@ BEGIN
         	AND v.Numero_max_pessoas >= NumPassageiro;	 
 RETURN ccarona;	 
 END; 
-
-ALTER PROCEDURE [dbo].[BuscarCarona]
-    @Cidade_saida NVARCHAR(100),
-    @DataSaida DATE,
-    @Cidade_chegada NVARCHAR(100),
-    @NumPassageiro INT
-AS
-BEGIN
-    SELECT 
-        pis.cidade,
-		piv.cidade_chegada,
-        v.data_hora_saida,  
-        v.numero_max_pessoas, 
-        v.valor  
-    FROM viagem v
-    JOIN ponto_viagem piv ON v.ID_viagem = piv.ID_viagem
-	JOIN pontos_intermediarios pis ON piv.CEP_ponto = pis.CEP
-    WHERE pis.cidade = @Cidade_saida 
-	  AND piv.cidade_chegada = @Cidade_chegada
-      AND CONVERT(DATE, v.data_hora_saida, 120) >= @DataSaida
-      AND v.Numero_max_pessoas >= @NumPassageiro;
-END;
-
 */
 
 
 --Busca Carona 
+-Busca Carona 
 CREATE PROCEDURE BuscarCarona
     @Cidade_saida NVARCHAR(100),
+    @DataSaida DATE,
     @DataSaida VARCHAR(100),
     @Cidade_chegada NVARCHAR(100),
     @NumPassageiro INT
 AS
 BEGIN
-    SELECT 
-        pis_chegada.cidade AS cidade_chegada,
-        pis_saida.cidade AS cidade_saida,  
-        v.data_hora_saida,  
-        v.numero_max_pessoas, 
-        v.valor  
-	
-    FROM Viagem v
     JOIN ponto_intermediario_viagem piv ON v.ID_viagem = piv.ID_viagem
-	
-	JOIN pontos_intermediarios pis_chegada ON pis_chegada.CEP = piv.CEP_ponto
-    	WHERE pis_chegada.cidade IN (@Cidade_saida)
-     	AND v.data_hora_saida = convert(varchar, @DataSaida, 103)
-        AND v.Numero_max_pessoas >= @NumPassageiroJOIN
-	
-	JOIN pontos_intermediarios pis_saida ON pis_saida.CEP = piv.CEP_ponto
-    	WHERE pis_saida.cidade IN (@Cidade_chegada)
-     	AND v.data_hora_saida = convert(varchar, @DataSaida, 103)
-        AND v.Numero_max_pessoas >= @NumPassageiro;
+    JOIN pontos_intermediarios pis ON pis.CEP = piv.CEP_ponto
+    WHERE pis.cidade IN (@Cidade_saida, @Cidade_chegada)
+      AND v.data_hora_saida = @DataSaida
+      AND v.data_hora_saida = convert(varchar, @DataSaida, 103)
+      AND v.Numero_max_pessoas >= @NumPassageiro;
 END;
