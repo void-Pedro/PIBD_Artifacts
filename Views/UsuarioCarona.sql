3ea7ea804179a417beb6e50b3c9141c1e7a0609d
Annalice <101228884+Annalicefs@users.noreply.github.com>
2024-09-18 12:56:11 -0300

    Update UsuarioCarona.sql

--- Views/UsuarioCarona.sql
+++ Views/UsuarioCarona.sql
@@ -1,7 +1,11 @@
 --View de pessoas que estão em uma carona
 
-CREATE OR REPLACE VIEW UsuarioCarona AS
-	SELECT v.ID_viagem, Nome, Nota_caronista, Nota_caroneiro
-	FROM Viagem v, Caroneiro_viagem cv, usuario_geral ug
+CREATE VIEW UsuarioCarona AS
+	SELECT 
+		v.ID_viagem, 
+		ug.nome_completo,
+		cv.nota_caronista, 
+		cv.nota_caroneiro
+	FROM Viagem v, caroneiro_viagem cv, usuario_geral ug
 	WHERE cv.ID_viagem = v.ID_viagem
-		AND ug.ID = cv.ID_caroneiro;
+		AND ug.ID = cv.caroneiro_ID;
