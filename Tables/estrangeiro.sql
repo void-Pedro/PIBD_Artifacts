3d7e459603f1764f56a7daab84e90eff2cb58bd7
Alessandro André Pérez Rodríguez <108166086+aaprodri@users.noreply.github.com>
2024-09-18 09:11:58 -0300

    Update estrangeiro.sql

--- Tables/estrangeiro.sql
+++ Tables/estrangeiro.sql
@@ -1,9 +1,10 @@
 --Tabela Estrangeiro
 
 CREATE TABLE IF NOT EXISTS estrangeiro (
-	ID_usuario INT NOT NULL,
+	ID_usuario INT IDENTITY (1,1) NOT NULL,
 	validade_passaporte VARCHAR(50) NOT NULL,
 	numero_passaporte VARCHAR(50) NOT NULL,
+	Senha VARCHAR(50) NOT NULL,
 
 	PRIMARY KEY(numero_passaporte),
 	FOREIGN KEY(ID_usuario) REFERENCES usuario_geral(ID)
