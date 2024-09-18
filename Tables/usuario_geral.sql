492ee8ff81d81d762396ba6edfec202ede0dd656
Alessandro André Pérez Rodríguez <108166086+aaprodri@users.noreply.github.com>
2024-09-18 12:03:51 -0300

    Update usuario_geral.sql

--- Tables/usuario_geral.sql
+++ Tables/usuario_geral.sql
@@ -2,14 +2,15 @@ CREATE TABLE IF NOT EXISTS  usuario_geral  (
    ID  INT AUTO_INCREMENT,
    rede_social varchar(100),
 
-   DDI  INT NOT NULL,
-   DDD  INT NOT NULL,
-   num_telefone  INT NOT NULL,
+   DDI  INT,
+   DDD  INT,
+   num_telefone  INT,
 
-   nome  VARCHAR(100) NOT NULL,
-   sobrenome  VARCHAR(100) NOT NULL,
-   data_nascimento  VARCHAR(100) NOT NULL,
-   sexo VARCHAR(100) NOT NULL,
+   nome  VARCHAR(100),
+   sobrenome  VARCHAR(100),
+   data_nascimento  VARCHAR(100),
+   sexo VARCHAR(100),
+   cadastro_stat VARCHAR(50) NOT NULL,
    PRIMARY KEY ( ID )
 
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
